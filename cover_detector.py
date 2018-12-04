b98f6649fc12605f3a2e90d1757529f4992d3444
jannaescur <janna.escur@alu-etsetb.upc.edu>
2018-12-04 20:19:33 +0100

    compute distance for both melodies and chromas

--- cover_detector.py
+++ cover_detector.py
@@ -29,10 +29,12 @@ class Detector(object):
 		#For melodies, subseq= True
 		#For chromas, subseq = False
 		D, wp = librosa.sequence.dtw(feat_song[1000:2000], feat_query[1000:2000], subseq=subseq)
-		print('feat song shape:', feat_song.shape)
-		print('query song shape:', feat_query.shape)
-		print('wp shape:', wp.shape)
+		#print('feat song shape:', feat_song.shape)
+		#print('query song shape:', feat_query.shape)
+		#print('wp shape:', wp.shape)
 		if subseq == True: #melodies
+			feat_song = np.expand_dims(feat_song, axis=1)
+			feat_query = np.expand_dims(feat_query, axis=1)
 			dist = self.get_dist(feat_song[wp[0,:]], feat_query[wp[1,:]])
 		if subseq == False: #chromas
 			dist = self.get_dist(feat_song[:,wp[:,0]], feat_query[:,wp[:,1]])
@@ -40,8 +42,10 @@ class Detector(object):
 
 	def get_dist(self, x, y):
 		# Efficient distance computation no loops ;)
-		return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
-
+		#return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
+		dist = -2 * np.dot(x, y.T) + np.sum(x**2, axis=1) + np.sum(y**2, axis=1)
+		dist = np.sum(dist)
+		return dist
 
 
 
@@ -53,18 +57,18 @@ melody_1 = data_1['melody']
 chroma_1 = data_1['chroma']
 
 #load file 1
-data_2 = np.load('Tests/lloyd_cole_and_the_commotions+Rattlesnakes+03-Rattlesnakes.npy').item()
+data_2 = np.load('Tests/creedence_clearwater_revival+Live_in_Europe+10-Proud_Mary.npy').item()
 melody_2 = data_2['melody']
 chroma_2 = data_2['chroma']
 
-
-
+#'Tests/lloyd_cole_and_the_commotions+Rattlesnakes+03-Rattlesnakes.npy'
 detector =Detector()
-
-#Comparing melodies
-dist_melody = detector.compare(melody_1, melody_2, subseq = True)
-print('distancia melody:', dist_melody)
 
 #Comparing chromas
 dist_chroma = detector.compare(chroma_1, chroma_2, subseq = False)
 print('distancia chroma:', dist_chroma)
+#Comparing melodies
+dist_melody = detector.compare(melody_1, melody_2, subseq = True)
+print('distancia melody:', dist_melody)
+
+
