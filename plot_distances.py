866413d2d740b94a1b57e740a8d01a6de71c6107
jorditruji <you@example.com>
2018-12-18 22:08:02 +0100

    before merge

--- plot_distances.py
+++ plot_distances.py
@@ -18,11 +18,31 @@ def load_dist (path):
 intra_dist_mel,intra_dist_chroma = load_dist('intra_dist/*.npy')
 inter_dist_mel, inter_dist_chroma = load_dist('inter_dist/*.npy')
 
-print intra_dist_chroma
+max_ = np.max(np.concatenate((np.array(inter_dist_mel),np.array(intra_dist_mel)), axis = 0))
+min_ = np.min(np.concatenate((np.array(inter_dist_mel),np.array(intra_dist_mel)), axis = 0))
+margin = max_ - min_
+bins = np.arange(min_, max_,margin/34 )
+plt.figure()
+plt.hist(inter_dist_mel,  bins=bins, alpha=0.5,color = 'red' ,label='inter-class')
+plt.hist(intra_dist_mel, bins=bins, alpha=0.5, label='intra-class', color = 'blue')
 
-
-plt.hist(intra_dist_mel, bins=24, alpha=0.5, label='intra-class', color = 'blue')
-plt.hist(inter_dist_mel+0*np.ones(len(inter_dist_mel)),  bins=24, alpha=0.5,color = 'red' ,label='inter-class')
 plt.legend(loc='upper right')
+
+plt.title("Distribution (n_pairs = 160)")
+
 plt.show()
 
+'''
+plt.subplot(2, 1, 1)
+plt.hist(intra_dist_mel, bins=24, alpha=0.5, label='intra-class', color = 'blue')
+plt.legend(loc='upper right')
+plt.title("Intra-class 2nd order distance distribution (n_pairs = 40)")
+
+plt.subplot(2, 1, 2)
+plt.hist(inter_dist_mel,  bins=24, alpha=0.5,color = 'red' ,label='inter-class')
+plt.legend(loc='upper right')
+plt.title("Inter-class 2nd order distance distribution (n_pairs = 40)")
+
+plt.show()
+
+'''
